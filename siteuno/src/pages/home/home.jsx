d8e086a797e13e673f8e946fa181d44def0ac15c
vini <viniciusicmsc@gmail.com>
2024-07-11 21:48:04 -0300

    home

--- siteuno/src/pages/home/home.jsx
+++ siteuno/src/pages/home/home.jsx
@@ -1,23 +1,18 @@
 import './home.css';
-import { Link } from 'react-router-dom'
+import HeaderNav from '../../components/header/header';
 
 function Home() {
     return (
         <div className="homepage">
-            <header className="homepage-header">
-                <h1>Bem-vindo ao Mundo do Uno!</h1>
-                <Link to='./sobre'>Sobre</Link>
-                <Link to='./comoJogar'>Como jogar</Link>
-                <Link to='./regras'>Regras</Link>
-            </header>
+            <HeaderNav />
             <main className="homepage-main">
                 <section className="intro">
                     <h2>Sobre o Jogo</h2>
                     <p>Uno é um jogo de cartas divertido e emocionante para todas as idades!</p>
                 </section>
-               
-               <section className='img-box'>
-               </section>
+
+                <section className='img-box'>
+                </section>
             </main>
             <footer className="homepage-footer">
                 <p>&copy; 2024 Mundo do Uno. Todos os direitos reservados.</p>
