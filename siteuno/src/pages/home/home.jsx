fb78a2259e2474c755d40377411c96376d065a32
vini <viniciusicmsc@gmail.com>
2024-07-11 21:42:44 -0300

    home

--- siteuno/src/pages/home/home.jsx
+++ siteuno/src/pages/home/home.jsx
@@ -1,7 +1,29 @@
-export default function Home(){
+import './home.css';
+import { Link } from 'react-router-dom'
+
+function Home() {
     return (
-        <>
-            <h1>HOME</h1>
-        </>
-    )
+        <div className="homepage">
+            <header className="homepage-header">
+                <h1>Bem-vindo ao Mundo do Uno!</h1>
+                <Link to='./sobre'>Sobre</Link>
+                <Link to='./comoJogar'>Como jogar</Link>
+                <Link to='./regras'>Regras</Link>
+            </header>
+            <main className="homepage-main">
+                <section className="intro">
+                    <h2>Sobre o Jogo</h2>
+                    <p>Uno é um jogo de cartas divertido e emocionante para todas as idades!</p>
+                </section>
+               
+               <section className='img-box'>
+               </section>
+            </main>
+            <footer className="homepage-footer">
+                <p>&copy; 2024 Mundo do Uno. Todos os direitos reservados.</p>
+            </footer>
+        </div>
+    );
 }
+
+export default Home;
